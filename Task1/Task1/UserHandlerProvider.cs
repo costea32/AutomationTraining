939f7d15e36e31821f312089be41a97e3753ccb7
costea32 <costea32@gmail.com>
2014-01-28 10:33:24 +0000

    task1 created
    task1 for automation training created

---
+++ Task1/Task1/UserHandlerProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1
+{
+    public class UserHandlerProvider
+    {
+        public IUserHandler GetHandler()
+        {
+            throw new NotImplementedException("this method will be implemented later");
+        }
+    }
+}
