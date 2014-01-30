a5f4b049fbbd7c2e7cd5e670449c8adfec37ad2f
Stefan-Coroli <stepka.k@gmail.com>
2014-01-30 13:58:03 +0000

    some negative tests

--- Task1/IUserHandlerTests/IUserHandlerTests.cs
+++ Task1/IUserHandlerTests/IUserHandlerTests.cs
@@ -10,19 +10,22 @@ namespace IUserHandlerTests
     public class IUserHandlerTests
     {
 
+        //
+        // Positive tests
+        //
+
         [TestMethod()]
         public void PositiveUserCountTest()
         {
             UserHandlerProvider handlerProvider = new UserHandlerProvider();
             IUserHandler handler = handlerProvider.GetHandler();
 
-            User user1 = new User("firstName1", "lastName1", 31);
-            User user2 = new User("firstName2", "lastName2", 32);
-
-            handler.Users.Add(user1);
-            handler.Users.Add(user2);
-
-            Assert.AreEqual(2, handler.UserCount);
+            for (int i = 0; i <= 10; i++)
+            {
+                User user = new User("firstName" + i.ToString(), "lastName" + i.ToString(), i);
+                handler.Users.Add(user);
+                Assert.AreEqual(i + 1, handler.UserCount);
+            }
         }
 
         [TestMethod()]
@@ -36,6 +39,21 @@ namespace IUserHandlerTests
             handler.AddUser(user1);
 
             Assert.AreEqual(1, handler.Users.Count);
+        }
+
+        [TestMethod()]
+        public void PositiveTestThatUserIsAddedCorrectly()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+
+            User user1 = new User("firstName", "lastName", 31);
+
+            handler.AddUser(user1);
+
+            Assert.AreEqual<string>("firstName", handler.Users[0].FirstName);
+            Assert.AreEqual<string>("lasttName", handler.Users[0].LastName);
+            Assert.AreEqual(31, handler.Users[0].Age);
         }
 
         [TestMethod()]
@@ -70,9 +88,16 @@ namespace IUserHandlerTests
             handler.Users.Add(user1);
             handler.Users.Add(user2);
 
+            Assert.AreEqual(handler.Users.Count, 2);
+
             handler.ClearData();
 
             Assert.AreEqual(handler.Users, null);
+
+            handler.Users.Add(user1);
+            handler.Users.Add(user2);
+
+            Assert.AreEqual(handler.Users.Count, 2);
         }
 
         [TestMethod()]
@@ -91,20 +116,100 @@ namespace IUserHandlerTests
         }
 
         [TestMethod()]
-        public void PositiveGetUserByNameTest()
+        public void PositiveGetUsersByAgeTest()
         {
             UserHandlerProvider handlerProvider = new UserHandlerProvider();
             IUserHandler handler = handlerProvider.GetHandler();
 
             User user1 = new User("firstName1", "lastName1", 31);
+            User user2 = new User("firstName11", "lastName11", 31);
+            User user3 = new User("firstName2", "lastName2", 32);
 
             handler.Users.Add(user1);
+            handler.Users.Add(user2);
+            handler.Users.Add(user3);
 
-            User user2 = handler.GetUserByName("firstName1", "lastName1");
+            List<User> expectedList = new List<User>();
+            expectedList.Add(user1);
+            expectedList.Add(user2);
 
-            Assert.AreEqual(user1, user2);
+            Assert.AreEqual(expectedList, handler.GetUsersByAge(31));
         }
 
+        //
+        // Negative tests
+        //
+
+        [TestMethod()]
+        public void NegativeAddUserWithNoFirstAndLastNameTest()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+            
+            User user1 = new User(null, null, 31);
+
+            handler.AddUser(user1);
+
+            Assert.AreEqual(0, handler.Users.Count);
+        }
+
+        [TestMethod()]
+        public void NegativeAddUserWithNoFirstNameTest()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+
+            User user1 = new User(null, "lastName", 31);
+
+            handler.AddUser(user1);
+
+            Assert.AreEqual(0, handler.Users.Count);
+        }
+
+        [TestMethod()]
+        public void NegativeAddUserWithNoLastNameTest()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+
+            User user1 = new User("firstName", null, 31);
+
+            handler.AddUser(user1);
+
+            Assert.AreEqual(0, handler.Users.Count);
+        }
+
+        [TestMethod()]
+        public void NegativeAddUserWithInvalidAgeTest()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+
+            User user1 = new User("firstName1", "lastName1", 0);
+            User user2 = new User("firstName2", "lastName2", -10);
+            User user3 = new User("firstName3", "lastName3", 151);
+
+            handler.AddUser(user1);
+            handler.AddUser(user2);
+            handler.AddUser(user3);
+
+            Assert.AreEqual(0, handler.Users.Count);
+        }
+
+        [TestMethod()]
+        public void NegativeAddUsersWithSameFirstAndLastNameTest()
+        {
+            UserHandlerProvider handlerProvider = new UserHandlerProvider();
+            IUserHandler handler = handlerProvider.GetHandler();
+
+            User user1 = new User("firstName", "lastName", 31);
+            User user2 = new User("firstName", "lastName", 32);
+
+            handler.AddUser(user1);
+            handler.AddUser(user2);
+
+            Assert.AreEqual(1, handler.Users.Count);
+        }
 
 
     }
