00ffabaf435c683b19c36b23458e9a8d4d16ac54
costea32 <costea32@gmail.com>
2014-01-31 11:26:28 +0000

    Revert "Some test"
    This reverts commit cdac0c842389e7941c4db0c597fd9cd463861164.

--- Task1/Task1/UserHandlerProvider.cs
+++ Task1/Task1/UserHandlerProvider.cs
@@ -5,55 +5,11 @@ using System.Text;
 
 namespace Task1
 {
-    class UserHandlerExample : IUserHandler
-    {
-        public UserHandlerExample()
-        {
-            userCount = 0;
-            users = new List<User>();
-        }
-
-        int userCount;
-        List<User> users;
-
-        public int UserCount
-        {
-            get { return userCount; }
-            private set { userCount = value; }
-        }
-
-        public void AddUser(User user)
-        {
-            users.Add(user);
-        }
-
-        public List<User> Users
-        {
-            get { return users; }
-            private set { users = value; }
-        }
-
-        public void ClearData()
-        {
-            users.Clear();
-        }
-
-        public User GetUserByName(string firstName, string lastName)
-        {
-            return users.FirstOrDefault(x => (x.FirstName.Equals(firstName) && x.LastName.Equals(lastName))) ?? null;
-        }
-
-        public List<User> GetUsersByAge(int age)
-        {
-            return users.FindAll(x => x.Age == age) ?? null;
-        }
-    }
-
     public class UserHandlerProvider
     {
         public IUserHandler GetHandler()
         {
-            return new UserHandlerExample();
+            throw new NotImplementedException("this method will be implemented later");
         }
     }
 }
