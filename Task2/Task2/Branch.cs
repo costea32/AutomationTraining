25c4494bce488cd78b83d872fa8a2151985cbbd7
alina-goncearenco <alina.goncharenco@gmail.com>
2014-02-04 15:21:46 +0000

    Task2 project created

---
+++ Task2/Task2/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Branch
+    {
+        public string branchName;
+        public int behind;
+        public int ahead;
+        // to add one more member
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenQA.Selenium;
+//using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "alina-goncearenco";
+            string password = "MyPassword1";
+            
+//            IWebDriver driver = new InternetExplorerDriver(@"C:\Users\goncharenkoa\Downloads\IEDriverServer_x64_2.39.0");
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("www.github.com/login");            
+
+            //Need to close fiddler!
+
+            IWebElement login = driver.FindElement(By.Id("login_field"));
+            login.SendKeys(username);
+            login.FindElement(By.Id("password"));
+            login.SendKeys(password);
+            login.SendKeys(Keys.Enter);
+
+            driver.Navigate().GoToUrl("https://github.com/costea32/AutomationTraining/branches");
+
+            driver.Close();
+
+            Console.WriteLine("\nTest finished!");
+            Console.ReadLine();
+        }
+    }
+}
