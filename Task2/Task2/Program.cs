cd5ac1f82c230ad9ac1687d4e9a66d4418c690d3
alina-goncearenco <alina.goncharenco@gmail.com>
2014-02-06 00:35:51 +0200

    Count branches added

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -32,17 +32,32 @@ namespace Task2
 
             driver.Navigate().GoToUrl("https://github.com/costea32/AutomationTraining/branches");
 
-            ICollection<IWebElement> table = driver.FindElements(By.XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]")); //XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]")
-            List<IWebElement> elements = table.ToList();
+//            ICollection<IWebElement> table = driver.FindElements(By.XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]")); //XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]")
+//            List<IWebElement> elements = table.ToList();
 
+//            int countBranches = driver.FindElements(By.XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]/tbody/tr")).Count;
+            int countBranches = driver.FindElements(By.ClassName("css-truncate")).Count;
+            
             List<Branch> branches = new List<Branch>();
-/*
-            foreach (IWebElement el in elements)
+
+            string bname;
+            int bbehind;
+            int bahead;
+
+            for (int i = 0; i < countBranches; i++ )
             {
-                el.GetAttribute()
+                bname = driver.FindElement(By.XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]/tbody/tr[" + (i+1) + "]/td[@class = 'name']/h3/a")).Text;
+                bbehind = Int32.Parse(driver.FindElement(By.XPath("/html/body/div/div[3]/div[2]/div/div[2]/table[2]/tbody/tr[" + (i+1) + "]/td[@class = 'state-widget']/div/span[@class = 'behind']/em")).Text.Substring(0,1));
+
             }
-*/
-            driver.Close();
+
+                /*
+                            foreach (IWebElement el in elements)
+                            {
+                                el.GetAttribute()
+                            }
+                */
+                driver.Close();
 
             Console.WriteLine("\nTest finished!");
             Console.ReadLine();
