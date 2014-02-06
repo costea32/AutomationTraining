33431a272315aa4e73ab1b4871e22bd8eb6b4346
RCut <r.cutasevich@gmail.com>
2014-02-06 12:29:17 +0200

    R

---
+++ Task2/SeleniumUITest/GitHubTableTestingProject/CodedUITest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+
+using OpenQA;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace GitHubTableTestingProject
+{
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            var driver = new FirefoxDriver();
+
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+        }
+
+        #endregion
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+    }
+}
---
+++ Task2/SeleniumUITest/ObjectMap/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenQA;
+using OpenQA.Selenium.Firefox;
+
+namespace ObjectMap
+{
+    public class Table
+    {
+
+    }
+}
