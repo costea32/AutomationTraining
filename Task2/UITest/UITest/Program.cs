96350ca6512a845b76936fb940065ec38f7888ff
ikulpin <i_kulpin@mail.ru>
2014-02-06 13:40:51 +0000

    moving

--- Task2/UITest/UITest/Program.cs
+++ Task2/UITest/UITest/Program.cs
@@ -14,8 +14,21 @@ namespace UITest
     {
         static void Main(string[] args)
         {
+            //string[] branches;
+            List<ChromeWebElement> elements = new List<ChromeWebElement>();
+
             IWebDriver driver = new ChromeDriver(@"C:\Users\culpini\Documents\chromedriver_win32");
-            driver.Navigate().GoToUrl("http://mail.ru");
+            try
+            {
+                driver.Navigate().GoToUrl("https://github.com/costea32/AutomationTraining/branches");
+                IWebElement element = driver.FindElement(By.XPath("//*[@id=\"js-repo-pjax-container\"]/table[2]/tbody/tr[1]/td[1]/h3/a"));
+                string str = element.GetAttribute("href");
+                Console.WriteLine(str);
+            }
+            finally
+            {
+                //driver.Close();
+            }
         }
     }
 }
