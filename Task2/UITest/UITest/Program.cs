d70c503cc5ffa2bf6bcc08e6981ec07330cd5f3d
ikulpin <i_kulpin@mail.ru>
2014-02-06 11:32:26 +0000

    Task 2 created

---
+++ Task2/UITest/UITest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace UITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\culpini\Documents\chromedriver_win32");
+            driver.Navigate().GoToUrl("http://mail.ru");
+        }
+    }
+}
